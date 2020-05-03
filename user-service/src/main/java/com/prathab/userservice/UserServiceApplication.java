045eb23606aad294a3713c9b2425f15c04914f77
Prathab <jm.prathab@gmail.com>
2020-05-03 12:52:34 +0530

    Implemented most of the functions and should work well with in most of the positive cases.
    Closes #1

--- user-service/src/main/java/com/prathab/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/prathab/userservice/UserServiceApplication.java
@@ -18,11 +18,21 @@ package com.prathab.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
+  }
+
+  @Bean
+  public PasswordEncoder getPasswordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
