14ec6f199b40bb99c2a528b21d28904aeb4e66b8
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-07-29 17:36:19 +0300

    fix wildcard imports

--- src/main/java/com/myhome/domain/HouseMember.java
+++ src/main/java/com/myhome/domain/HouseMember.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @AllArgsConstructor
--- src/main/java/com/myhome/domain/HouseMemberDocument.java
+++ src/main/java/com/myhome/domain/HouseMemberDocument.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.FetchType;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
 
 @Entity
 @AllArgsConstructor
