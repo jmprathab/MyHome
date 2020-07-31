eda100e47da19efffdba5d0febef142e21846346
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-07-31 17:17:18 +0300

    add unit tests for HouseMemberDocumentService

---
+++ src/test/java/com/myhome/services/HouseMemberDocumentServiceTest.java
@@ -0,0 +1,135 @@
+package com.myhome.services;
+
+import com.myhome.domain.HouseMember;
+import com.myhome.domain.HouseMemberDocument;
+import com.myhome.repositories.HouseMemberDocumentRepository;
+import com.myhome.repositories.HouseMemberRepository;
+import com.myhome.services.springdatajpa.HouseMemberDocumentSDJpaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+public class HouseMemberDocumentServiceTest {
+
+    private static final String MEMBER_ID = "test-member-id";
+    private static final String MEMBER_NAME = "test-member-name";
+    private static final HouseMemberDocument MEMBER_DOCUMENT = new HouseMemberDocument("test-file-name", new byte[0]);
+    
+    @Mock
+    private HouseMemberRepository houseMemberRepository;
+    
+    @Mock
+    private HouseMemberDocumentRepository houseMemberDocumentRepository;
+    
+    @InjectMocks
+    private HouseMemberDocumentSDJpaService houseMemberDocumentService;
+
+    private byte[] getImage(int height, int width) throws IOException {
+        BufferedImage documentImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        try(ByteArrayOutputStream imageBytesStream = new ByteArrayOutputStream()) {
+            ImageIO.write(documentImage, "jpg", imageBytesStream);
+            return imageBytesStream.toByteArray();
+        }
+    }
+
+    @BeforeEach
+    private void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    void findMemberDocumentSuccess() {
+        // given
+        HouseMember testMember = new HouseMember(MEMBER_ID, MEMBER_DOCUMENT, MEMBER_NAME, null);
+        given(houseMemberRepository.findByMemberId(MEMBER_ID))
+                .willReturn(Optional.of(testMember));
+        // when
+        Optional<HouseMemberDocument> houseMemberDocument = houseMemberDocumentService.findHouseMemberDocument(MEMBER_ID);
+        // then
+        assertTrue(houseMemberDocument.isPresent());
+        assertEquals(MEMBER_DOCUMENT, houseMemberDocument.get());
+        verify(houseMemberRepository).findByMemberId(MEMBER_ID);
+    }
+
+    @Test
+    void findMemberDocumentNoDocumentPresent() {
+        // given
+        HouseMember testMember = new HouseMember(MEMBER_ID, null, MEMBER_NAME, null);
+        given(houseMemberRepository.findByMemberId(MEMBER_ID))
+                .willReturn(Optional.of(testMember));
+        // when
+        Optional<HouseMemberDocument> houseMemberDocument = houseMemberDocumentService.findHouseMemberDocument(MEMBER_ID);
+        // then
+        assertFalse(houseMemberDocument.isPresent());
+        //verify(houseMemberRepository).findByMemberId(MEMBER_ID);
+    }
+
+    @Test
+    void findMemberDocumentMemberNotExists() {
+        // given
+        given(houseMemberRepository.findByMemberId(MEMBER_ID))
+                .willReturn(Optional.empty());
+        // when
+        Optional<HouseMemberDocument> houseMemberDocument = houseMemberDocumentService.findHouseMemberDocument(MEMBER_ID);
+        // then
+        assertFalse(houseMemberDocument.isPresent());
+        verify(houseMemberRepository).findByMemberId(MEMBER_ID);
+    }
+
+    @Test
+    void deleteMemberDocumentSuccess() {
+        // given
+        HouseMember testMember = new HouseMember(MEMBER_ID, MEMBER_DOCUMENT, MEMBER_NAME, null);
+        given(houseMemberRepository.findByMemberId(MEMBER_ID))
+                .willReturn(Optional.of(testMember));
+        // when
+        boolean isDocumentDeleted = houseMemberDocumentService.deleteHouseMemberDocument(MEMBER_ID);
+        // then
+        assertTrue(isDocumentDeleted);
+        assertNull(testMember.getHouseMemberDocument());
+        verify(houseMemberRepository).findByMemberId(MEMBER_ID);
+        verify(houseMemberRepository).save(testMember);
+    }
+
+    @Test
+    void deleteMemberDocumentNoDocumentPresent() {
+        // given
+        HouseMember testMember = new HouseMember(MEMBER_ID, null, MEMBER_NAME, null);
+        given(houseMemberRepository.findByMemberId(MEMBER_ID))
+                .willReturn(Optional.of(testMember));
+        // when
+        boolean isDocumentDeleted = houseMemberDocumentService.deleteHouseMemberDocument(MEMBER_ID);
+        // then
+        assertTrue(isDocumentDeleted);
+        assertNull(testMember.getHouseMemberDocument());
+        verify(houseMemberRepository).findByMemberId(MEMBER_ID);
+    }
+
+    @Test
+    void deleteMemberDocumentMemberNotExists() {
+        // given
+        given(houseMemberRepository.findByMemberId(MEMBER_ID))
+                .willReturn(Optional.empty());
+        // when
+        boolean isDocumentDeleted = houseMemberDocumentService.deleteHouseMemberDocument(MEMBER_ID);
+        // then
+        assertFalse(isDocumentDeleted);
+        verify(houseMemberRepository).findByMemberId(MEMBER_ID);
+    }
+}
