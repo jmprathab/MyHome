a4640b96ba695029e81e20b2db126c7f910bdd62
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-08-09 17:26:55 +0300

    clean up

--- src/test/java/com/myhome/services/unit/UserSDJpaServiceTest.java
+++ src/test/java/com/myhome/services/unit/UserSDJpaServiceTest.java
@@ -24,7 +24,6 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
