71e7c6b8a77ab09292186f07c99135a48d582e1b
Edward Lynch-Milner <18222021@studentmail.ul.ie>
2020-08-12 19:19:20 +0100

    Removed unnecessary import

--- src/test/java/com/myhome/controllers/unit/HealthCheckControllerTest.java
+++ src/test/java/com/myhome/controllers/unit/HealthCheckControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 
 public class HealthCheckControllerTest {
 
