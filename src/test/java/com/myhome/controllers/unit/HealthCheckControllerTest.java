8a6b3b50fa095af550356d36310ccb25cd36ab5c
Edward Lynch-Milner <18222021@studentmail.ul.ie>
2020-08-12 19:17:48 +0100

    Made statusCheck return a ResponseEntity and applied the given when then pattern to the unit test

--- src/test/java/com/myhome/controllers/unit/HealthCheckControllerTest.java
+++ src/test/java/com/myhome/controllers/unit/HealthCheckControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 public class HealthCheckControllerTest {
 
