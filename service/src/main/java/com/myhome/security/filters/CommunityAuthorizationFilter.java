2bedf9daa30fa8482cd21eea5c348f421c50a149
myself <myself@myself.neverland>
2020-10-09 22:05:40 +0200

    Issue #152: code styles, code review edits

--- service/src/main/java/com/myhome/security/filters/CommunityAuthorizationFilter.java
+++ service/src/main/java/com/myhome/security/filters/CommunityAuthorizationFilter.java
@@ -11,15 +11,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CommunityAuthorizationFilter extends BasicAuthenticationFilter {
     private final CommunityService communityService;
-    private final String uuidPattern = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}";
-    private final Pattern addAmenityRequestPattern = Pattern.compile("/communities/" + uuidPattern + "/amenities");
+    private static final String UUID_PATTERN = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}";
+    private static final Pattern ADD_AMENITY_REQUEST_PATTERN = Pattern.compile("/communities/" + UUID_PATTERN + "/amenities");
 
 
     public CommunityAuthorizationFilter(AuthenticationManager authenticationManager,
@@ -32,10 +30,10 @@ public class CommunityAuthorizationFilter extends BasicAuthenticationFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain chain) throws IOException, ServletException {
 
-        Matcher urlMatcher = addAmenityRequestPattern.matcher(request.getRequestURI());
+        Matcher urlMatcher = ADD_AMENITY_REQUEST_PATTERN.matcher(request.getRequestURI());
 
         if (urlMatcher.find() && !isUserCommunityAdmin(request)) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return;
         }
 
@@ -43,24 +41,15 @@ public class CommunityAuthorizationFilter extends BasicAuthenticationFilter {
     }
 
     private boolean isUserCommunityAdmin(HttpServletRequest request) {
-        String userId = (String) SecurityContextHolder
-                .getContext().getAuthentication().getPrincipal();
-        String communityId = request
-                .getRequestURI().split("/")[2];
-        Optional<List<User>> optional = communityService
-                .findCommunityAdminsById(communityId, null);
+        String userId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String communityId = request.getRequestURI().split("/")[2];
 
-        if (optional.isPresent()) {
-            List<User> communityAdmins = optional.get();
-            User admin = communityAdmins
-                    .stream()
-                    .filter(communityAdmin -> communityAdmin.getUserId().equals(userId))
-                    .findFirst()
-                    .orElse(null);
-
-            return admin != null;
-        }
-
-        return false;
+        return communityService.findCommunityAdminsById(communityId, null)
+                .flatMap(admins -> admins.stream()
+                        .map(User::getUserId)
+                        .filter(userId::equals)
+                        .findFirst()
+                )
+                .isPresent();
     }
 }
