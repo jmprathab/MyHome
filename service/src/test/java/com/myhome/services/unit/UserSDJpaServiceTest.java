2ef6d0eab2192ddb60bd3d641479869c845ddbc8
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-10-13 23:42:50 +0300

    Issue #151 Add new UserService tests

--- service/src/test/java/com/myhome/services/unit/UserSDJpaServiceTest.java
+++ service/src/test/java/com/myhome/services/unit/UserSDJpaServiceTest.java
@@ -19,15 +19,25 @@ package com.myhome.services.unit;
 import helpers.TestUtils;
 import com.myhome.controllers.dto.UserDto;
 import com.myhome.controllers.dto.mapper.UserMapper;
+import com.myhome.controllers.request.ForgotPasswordRequest;
 import com.myhome.domain.Community;
+import com.myhome.domain.MyHomeTokenType;
+import com.myhome.domain.SecurityToken;
 import com.myhome.domain.User;
+import com.myhome.repositories.SecurityTokenRepository;
 import com.myhome.repositories.UserRepository;
+import com.myhome.services.springdatajpa.MailSDJpaService;
+import com.myhome.services.springdatajpa.SecurityTokenSDJpaService;
 import com.myhome.services.springdatajpa.UserSDJpaService;
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -37,16 +47,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 class UserSDJpaServiceTest {
 
   private final String USER_ID = "test-user-id";
-  private final String USERNAME = "test-user-id";
-  private final String USER_EMAIL = "test-user-id";
-  private final String USER_PASSWORD = "test-user-id";
+  private final String USERNAME = "test-user-name";
+  private final String USER_EMAIL = "test-user-email";
+  private final String USER_PASSWORD = "test-user-password";
+  private final String NEW_USER_PASSWORD = "test-user-new-password";
+  private final String PASSWORD_RESET_TOKEN = "test-token";
+  private final int TOKEN_LIFETIME = 10000;
+  private final int EXPIRED_TOKEN_LIFETIME = -10000;
 
   @Mock
   private UserRepository userRepository;
@@ -54,6 +75,12 @@ class UserSDJpaServiceTest {
   private UserMapper userMapper;
   @Mock
   private PasswordEncoder passwordEncoder;
+  @Mock
+  private SecurityTokenSDJpaService securityTokenService;
+  @Mock
+  private MailSDJpaService mailService;
+  @Mock
+  private SecurityTokenRepository securityTokenRepository;
   @InjectMocks
   private UserSDJpaService userService;
 
@@ -186,6 +213,170 @@ class UserSDJpaServiceTest {
     verify(userRepository).findByUserIdWithCommunities(USER_ID);
   }
 
+  @Test
+  void requestResetPassword() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    User user = getDefaultUser();
+    SecurityToken testSecurityToken = getTestSecurityToken();
+    given(securityTokenService.createPasswordResetToken())
+        .willReturn(testSecurityToken);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.of(user));
+
+    // when
+    boolean passwordResetRequested = userService.requestResetPassword(forgotPasswordRequest);
+
+    // then
+    assertTrue(passwordResetRequested);
+    assertEquals(user.getPasswordResetToken(), testSecurityToken);
+    verify(securityTokenService).createPasswordResetToken();
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verify(userRepository).save(user);
+    verify(mailService).sendPasswordRecoverCode(user, testSecurityToken.getToken());
+  }
+
+  @Test
+  void requestResetPasswordUserNotExists() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    User user = getDefaultUser();
+    SecurityToken testSecurityToken = getTestSecurityToken();
+    given(securityTokenService.createPasswordResetToken())
+        .willReturn(testSecurityToken);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.empty());
+
+    // when
+    boolean passwordResetRequested = userService.requestResetPassword(forgotPasswordRequest);
+
+    // then
+    assertFalse(passwordResetRequested);
+    assertNotEquals(user.getPasswordResetToken(), testSecurityToken);
+    verify(securityTokenService).createPasswordResetToken();
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verify(userRepository, never()).save(user);
+    verifyNoInteractions(mailService);
+  }
+
+  @Test
+  void resetPassword() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    SecurityToken testSecurityToken = getTestSecurityToken();
+    User user = getDefaultUser();
+    user.setPasswordResetToken(testSecurityToken);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.of(user));
+    given(passwordEncoder.encode(forgotPasswordRequest.newPassword))
+        .willReturn(forgotPasswordRequest.newPassword);
+    when(userRepository.save(user))
+        .then(returnsFirstArg());
+
+    // when
+    boolean passwordChanged = userService.resetPassword(forgotPasswordRequest);
+
+    // then
+    assertTrue(passwordChanged);
+    assertEquals(user.getEncryptedPassword(), forgotPasswordRequest.newPassword);
+    assertNull(user.getPasswordResetToken());
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verify(securityTokenRepository).delete(testSecurityToken);
+    verify(passwordEncoder).encode(forgotPasswordRequest.newPassword);
+    verify(mailService).sendPasswordSuccessfullyChanged(user);
+  }
+
+  @Test
+  void resetPasswordUserNotExists() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    SecurityToken testSecurityToken = getTestSecurityToken();
+    User user = getDefaultUser();
+    user.setPasswordResetToken(testSecurityToken);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.empty());
+
+    // when
+    boolean passwordChanged = userService.resetPassword(forgotPasswordRequest);
+
+    // then
+    assertFalse(passwordChanged);
+    assertNotEquals(user.getEncryptedPassword(), forgotPasswordRequest.newPassword);
+    assertNotNull(user.getPasswordResetToken());
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verifyNoInteractions(securityTokenRepository);
+    verifyNoInteractions(passwordEncoder);
+    verifyNoInteractions(mailService);
+  }
+
+  @Test
+  void resetPasswordTokenExpired() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    SecurityToken testSecurityToken = getExpiredTestToken();
+    User user = getDefaultUser();
+    user.setPasswordResetToken(testSecurityToken);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.of(user));
+
+    // when
+    boolean passwordChanged = userService.resetPassword(forgotPasswordRequest);
+
+    // then
+    assertFalse(passwordChanged);
+    assertNotEquals(user.getEncryptedPassword(), forgotPasswordRequest.newPassword);
+    assertNotNull(user.getPasswordResetToken());
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verifyNoInteractions(securityTokenRepository);
+    verifyNoInteractions(passwordEncoder);
+    verifyNoInteractions(mailService);
+  }
+
+  @Test
+  void resetPasswordTokenNotExists() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    User user = getDefaultUser();
+    user.setPasswordResetToken(null);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.of(user));
+
+    // when
+    boolean passwordChanged = userService.resetPassword(forgotPasswordRequest);
+
+    // then
+    assertFalse(passwordChanged);
+    assertNotEquals(user.getEncryptedPassword(), forgotPasswordRequest.newPassword);
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verifyNoInteractions(securityTokenRepository);
+    verifyNoInteractions(passwordEncoder);
+    verifyNoInteractions(mailService);
+  }
+
+  @Test
+  void resetPasswordTokenNotMatches() {
+    // given
+    ForgotPasswordRequest forgotPasswordRequest = getForgotPasswordRequest();
+    SecurityToken testSecurityToken = getTestSecurityToken();
+    testSecurityToken.setToken("wrong-token");
+    User user = getDefaultUser();
+    user.setPasswordResetToken(testSecurityToken);
+    given(userRepository.findByEmailWithPasswordResetToken(forgotPasswordRequest.email))
+        .willReturn(Optional.of(user));
+
+    // when
+    boolean passwordChanged = userService.resetPassword(forgotPasswordRequest);
+
+    // then
+    assertFalse(passwordChanged);
+    assertNotEquals(user.getEncryptedPassword(), forgotPasswordRequest.newPassword);
+    assertNotNull(user.getPasswordResetToken());
+    verify(userRepository).findByEmailWithPasswordResetToken(forgotPasswordRequest.email);
+    verifyNoInteractions(securityTokenRepository);
+    verifyNoInteractions(passwordEncoder);
+    verifyNoInteractions(mailService);
+  }
+
   private UserDto getDefaultUserDtoRequest() {
     return UserDto.builder()
         .userId(USER_ID)
@@ -206,4 +397,29 @@ class UserSDJpaServiceTest {
         null
     );
   }
+
+  private User getDefaultUser() {
+    return getUserFromDto(getDefaultUserDtoRequest());
+  }
+
+  private ForgotPasswordRequest getForgotPasswordRequest() {
+    return new ForgotPasswordRequest(USER_EMAIL, PASSWORD_RESET_TOKEN, NEW_USER_PASSWORD);
+  }
+
+  private SecurityToken getTestSecurityToken(int lifetime) {
+    Date date = new Date();
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    cal.add(Calendar.SECOND, lifetime);
+    return new SecurityToken(MyHomeTokenType.RESET, PASSWORD_RESET_TOKEN, new Date(), cal.getTime());
+  }
+
+  private SecurityToken getTestSecurityToken() {
+    return getTestSecurityToken(TOKEN_LIFETIME);
+  }
+
+  private SecurityToken getExpiredTestToken() {
+    return getTestSecurityToken(EXPIRED_TOKEN_LIFETIME);
+  }
+
 }
