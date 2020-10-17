9feb7c0da8a39ef28341a3e17fbfd45ad3b0af27
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-10-17 19:02:58 +0300

    Issue #151 fix SecurityTokenSDJpaServiceTest

--- service/src/test/java/com/myhome/services/unit/SecurityTokenSDJpaServiceTest.java
+++ service/src/test/java/com/myhome/services/unit/SecurityTokenSDJpaServiceTest.java
@@ -1,7 +1,7 @@
 package com.myhome.services.unit;
 
-import com.myhome.domain.SecurityTokenType;
 import com.myhome.domain.SecurityToken;
+import com.myhome.domain.SecurityTokenType;
 import com.myhome.repositories.SecurityTokenRepository;
 import com.myhome.services.springdatajpa.SecurityTokenSDJpaService;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Date;
+import java.time.Duration;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,8 +24,7 @@ import static org.mockito.Mockito.when;
 
 public class SecurityTokenSDJpaServiceTest {
 
-  private final int TEST_TOKEN_LIFETIME_SECONDS = 60 * 60 * 24;
-  private final int TEST_TOKEN_LIFETIME_SECONDS_UNIX = TEST_TOKEN_LIFETIME_SECONDS * 1000;
+  private final Duration TEST_TOKEN_LIFETIME_SECONDS = Duration.ofDays(1);
 
   @Mock
   private SecurityTokenRepository securityTokenRepository;
@@ -48,14 +48,14 @@ public class SecurityTokenSDJpaServiceTest {
 
     // when
     SecurityToken actualSecurityToken = securityTokenSDJpaService.createSecurityToken(testTokenType, TEST_TOKEN_LIFETIME_SECONDS);
-    Date creationDate = actualSecurityToken.getCreationDate();
-    Date expiryDate = actualSecurityToken.getExpiryDate();
-    long lifetime = expiryDate.getTime() - creationDate.getTime();
+    LocalDate creationDate = actualSecurityToken.getCreationDate();
+    LocalDate expiryDate = actualSecurityToken.getExpiryDate();
+    Duration lifetime = Duration.between(creationDate.atStartOfDay(), expiryDate.atStartOfDay());
 
     // then
     assertEquals(actualSecurityToken.getTokenType(), testTokenType);
-    assertTrue(creationDate.before(expiryDate));
-    assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS_UNIX);
+    assertTrue(creationDate.isBefore(expiryDate));
+    assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS);
     assertNotNull(actualSecurityToken.getToken());
     verify(securityTokenRepository).save(any());
   }
@@ -68,14 +68,14 @@ public class SecurityTokenSDJpaServiceTest {
 
     // when
     SecurityToken actualSecurityToken = securityTokenSDJpaService.createPasswordResetToken();
-    Date creationDate = actualSecurityToken.getCreationDate();
-    Date expiryDate = actualSecurityToken.getExpiryDate();
-    long lifetime = expiryDate.getTime() - creationDate.getTime();
+    LocalDate creationDate = actualSecurityToken.getCreationDate();
+    LocalDate expiryDate = actualSecurityToken.getExpiryDate();
+    Duration lifetime = Duration.between(creationDate.atStartOfDay(), expiryDate.atStartOfDay());
 
     // then
     assertEquals(actualSecurityToken.getTokenType(), SecurityTokenType.RESET);
-    assertTrue(creationDate.before(expiryDate));
-    assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS_UNIX);
+    assertTrue(creationDate.isBefore(expiryDate));
+    assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS);
     assertNotNull(actualSecurityToken.getToken());
     verify(securityTokenRepository).save(any());
   }
