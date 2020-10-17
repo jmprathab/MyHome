3c5bf885d19933a5f5e4ff64bd1ae4cafd70fd4c
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-10-17 22:34:08 +0300

    Issue #151 fix build

--- service/src/main/java/com/myhome/services/springdatajpa/SecurityTokenSDJpaService.java
+++ service/src/main/java/com/myhome/services/springdatajpa/SecurityTokenSDJpaService.java
@@ -42,5 +42,5 @@ public class SecurityTokenSDJpaService implements SecurityTokenService {
     securityTokenRepository.save(token);
   }
 
-  private LocalDate getDateAfterDays(LocalDate date, Duration liveTime) { return date.plus(liveTime); }
+  private LocalDate getDateAfterDays(LocalDate date, Duration liveTime) { return date.plusDays(liveTime.toDays()); }
 }
