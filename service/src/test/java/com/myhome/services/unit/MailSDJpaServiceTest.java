67aa4b5721286aa6dd6c15aeac71f21dad4d2b8f
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-01-06 02:51:00 +0300

    Issue #196 fix tests

--- service/src/test/java/com/myhome/services/unit/MailSDJpaServiceTest.java
+++ service/src/test/java/com/myhome/services/unit/MailSDJpaServiceTest.java
@@ -1,5 +1,6 @@
 package com.myhome.services.unit;
 
+import com.myhome.domain.SecurityToken;
 import com.myhome.domain.User;
 import com.myhome.services.springdatajpa.MailSDJpaService;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,19 +8,30 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 
 class MailSDJpaServiceTest {
 
   @Mock
   private JavaMailSender mailSender;
+  @Mock
+  private ITemplateEngine emailTemplateEngine;
+  @Mock
+  private ResourceBundleMessageSource messageSource;
 
   @InjectMocks
   private MailSDJpaService mailSDJpaService;
@@ -27,13 +39,26 @@ class MailSDJpaServiceTest {
   @BeforeEach
   private void init() {
     MockitoAnnotations.initMocks(this);
+    ReflectionTestUtils.setField(mailSDJpaService, "username", "username");
+    ReflectionTestUtils.setField(mailSDJpaService, "passwordChangedMailTemplateName", "");
+    ReflectionTestUtils.setField(mailSDJpaService, "passwordRecoverMailTemplateName", "");
+    ReflectionTestUtils.setField(mailSDJpaService, "accountCreatedMailTemplateName", "");
+    ReflectionTestUtils.setField(mailSDJpaService, "accountConfirmedMailTemplateName", "");
+    ReflectionTestUtils.setField(mailSDJpaService, "port", "");
+    ReflectionTestUtils.setField(mailSDJpaService, "host", "");
+
   }
 
   @Test
   void sendPasswordRecoverCodeMailException() {
     // given
+    MimeMessage mimeMessage = new MimeMessage((Session)null);
     User user = getTestUser();
-    doThrow(mock(MailException.class)).when(mailSender).send((SimpleMailMessage) any());
+    given(emailTemplateEngine.process(eq(""), any(Context.class)))
+        .willReturn("HTML");
+    given(mailSender.createMimeMessage())
+        .willReturn(mimeMessage);
+    doThrow(MailSendException.class).when(mailSender).send(mimeMessage);
 
     // when
     boolean mailSent = mailSDJpaService.sendPasswordRecoverCode(user, "test-token");
@@ -45,11 +70,54 @@ class MailSDJpaServiceTest {
   @Test
   void sendPasswordSuccessfullyChangedMailException() {
     // given
+    MimeMessage mimeMessage = new MimeMessage((Session)null);
     User user = getTestUser();
-    doThrow(mock(MailException.class)).when(mailSender).send((SimpleMailMessage) any());
+    given(emailTemplateEngine.process(eq(""), any(Context.class)))
+        .willReturn("HTML");
+    given(mailSender.createMimeMessage())
+        .willReturn(mimeMessage);
+    doThrow(MailSendException.class).when(mailSender).send(mimeMessage);
 
     // when
     boolean mailSent = mailSDJpaService.sendPasswordSuccessfullyChanged(user);
+
+    // then
+    assertFalse(mailSent);
+  }
+
+  @Test
+  void sendEmailConfirmedMailException() {
+    // given
+    MimeMessage mimeMessage = new MimeMessage((Session)null);
+    User user = getTestUser();
+    given(emailTemplateEngine.process(eq(""), any(Context.class)))
+        .willReturn("HTML");
+    given(mailSender.createMimeMessage())
+        .willReturn(mimeMessage);
+    doThrow(MailSendException.class).when(mailSender).send(mimeMessage);
+
+    // when
+    boolean mailSent = mailSDJpaService.sendAccountConfirmed(user);
+
+    // then
+    assertFalse(mailSent);
+  }
+
+  @Test
+  void sendEmailCreatedMailException() {
+    // given
+    SecurityToken token = new SecurityToken();
+    token.setToken("token");
+    MimeMessage mimeMessage = new MimeMessage((Session)null);
+    User user = getTestUser();
+    given(emailTemplateEngine.process(eq(""), any(Context.class)))
+        .willReturn("HTML");
+    given(mailSender.createMimeMessage())
+        .willReturn(mimeMessage);
+    doThrow(MailSendException.class).when(mailSender).send(mimeMessage);
+
+    // when
+    boolean mailSent = mailSDJpaService.sendAccountCreated(user, token);
 
     // then
     assertFalse(mailSent);
