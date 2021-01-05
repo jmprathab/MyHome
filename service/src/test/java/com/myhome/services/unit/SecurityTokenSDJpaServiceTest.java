c88372795fb188a0dfde1bbd0086852204b51240
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-01-06 02:27:11 +0300

    Issue #211 fix and add SecurityToken tests

--- service/src/test/java/com/myhome/services/unit/SecurityTokenSDJpaServiceTest.java
+++ service/src/test/java/com/myhome/services/unit/SecurityTokenSDJpaServiceTest.java
@@ -2,8 +2,10 @@ package com.myhome.services.unit;
 
 import com.myhome.domain.SecurityToken;
 import com.myhome.domain.SecurityTokenType;
+import com.myhome.domain.User;
 import com.myhome.repositories.SecurityTokenRepository;
 import com.myhome.services.springdatajpa.SecurityTokenSDJpaService;
+import helpers.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -37,17 +39,21 @@ public class SecurityTokenSDJpaServiceTest {
     MockitoAnnotations.initMocks(this);
     ReflectionTestUtils.setField(securityTokenSDJpaService, "passResetTokenTime",
         TEST_TOKEN_LIFETIME_SECONDS);
+    ReflectionTestUtils.setField(securityTokenSDJpaService, "emailConfirmTokenTime",
+        TEST_TOKEN_LIFETIME_SECONDS);
   }
 
   @Test
   void createSecurityToken() {
     // given
+    User user = new User();
+    user.setUserId(TestUtils.General.generateUniqueId());
     SecurityTokenType testTokenType = SecurityTokenType.RESET;
     when(securityTokenRepository.save(any()))
         .then(returnsFirstArg());
 
     // when
-    SecurityToken actualSecurityToken = securityTokenSDJpaService.createSecurityToken(testTokenType, TEST_TOKEN_LIFETIME_SECONDS);
+    SecurityToken actualSecurityToken = securityTokenSDJpaService.createPasswordResetToken(user);
     LocalDate creationDate = actualSecurityToken.getCreationDate();
     LocalDate expiryDate = actualSecurityToken.getExpiryDate();
     Duration lifetime = Duration.between(creationDate.atStartOfDay(), expiryDate.atStartOfDay());
@@ -56,6 +62,7 @@ public class SecurityTokenSDJpaServiceTest {
     assertEquals(actualSecurityToken.getTokenType(), testTokenType);
     assertTrue(creationDate.isBefore(expiryDate));
     assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS);
+    assertEquals(user, actualSecurityToken.getTokenOwner());
     assertNotNull(actualSecurityToken.getToken());
     verify(securityTokenRepository).save(any());
   }
@@ -63,11 +70,13 @@ public class SecurityTokenSDJpaServiceTest {
   @Test
   void createPasswordResetToken() {
     // given
+    User user = new User();
+    user.setUserId(TestUtils.General.generateUniqueId());
     when(securityTokenRepository.save(any()))
         .then(returnsFirstArg());
 
     // when
-    SecurityToken actualSecurityToken = securityTokenSDJpaService.createPasswordResetToken();
+    SecurityToken actualSecurityToken = securityTokenSDJpaService.createPasswordResetToken(user);
     LocalDate creationDate = actualSecurityToken.getCreationDate();
     LocalDate expiryDate = actualSecurityToken.getExpiryDate();
     Duration lifetime = Duration.between(creationDate.atStartOfDay(), expiryDate.atStartOfDay());
@@ -76,6 +85,30 @@ public class SecurityTokenSDJpaServiceTest {
     assertEquals(actualSecurityToken.getTokenType(), SecurityTokenType.RESET);
     assertTrue(creationDate.isBefore(expiryDate));
     assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS);
+    assertEquals(user, actualSecurityToken.getTokenOwner());
+    assertNotNull(actualSecurityToken.getToken());
+    verify(securityTokenRepository).save(any());
+  }
+
+  @Test
+  void createEmailConfirmToken() {
+    // given
+    User user = new User();
+    user.setUserId(TestUtils.General.generateUniqueId());
+    when(securityTokenRepository.save(any()))
+        .then(returnsFirstArg());
+
+    // when
+    SecurityToken actualSecurityToken = securityTokenSDJpaService.createEmailConfirmToken(user);
+    LocalDate creationDate = actualSecurityToken.getCreationDate();
+    LocalDate expiryDate = actualSecurityToken.getExpiryDate();
+    Duration lifetime = Duration.between(creationDate.atStartOfDay(), expiryDate.atStartOfDay());
+
+    // then
+    assertEquals(actualSecurityToken.getTokenType(), SecurityTokenType.EMAIL_CONFIRM);
+    assertTrue(creationDate.isBefore(expiryDate));
+    assertEquals(lifetime, TEST_TOKEN_LIFETIME_SECONDS);
+    assertEquals(user, actualSecurityToken.getTokenOwner());
     assertNotNull(actualSecurityToken.getToken());
     verify(securityTokenRepository).save(any());
   }
