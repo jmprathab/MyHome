9bd6eccdf0421e132374c32331afc99615cfe772
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2021-02-17 22:44:33 +0100

    Issue #222: Add lombok to api module

--- api/src/main/java/com/myhome/utils/PageInfo.java
+++ api/src/main/java/com/myhome/utils/PageInfo.java
@@ -1,21 +1,20 @@
 package com.myhome.utils;
 
-import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class PageInfo {
   private final int currentPage;
   private final int pageLimit;
   private final int totalPages;
   private final long totalElements;
-
-  private PageInfo(int currentPage, int pageLimit, int totalPages, long totalElements) {
-    this.currentPage = currentPage;
-    this.pageLimit = pageLimit;
-    this.totalPages = totalPages;
-    this.totalElements = totalElements;
-  }
 
   public static PageInfo of(Pageable pageable, Page<?> page) {
     return new PageInfo(
@@ -24,30 +23,5 @@ public class PageInfo {
         page.getTotalPages(),
         page.getTotalElements()
     );
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    PageInfo pageInfo = (PageInfo) o;
-    return currentPage == pageInfo.currentPage
-        && pageLimit == pageInfo.pageLimit
-        && totalPages == pageInfo.totalPages
-        && totalElements == pageInfo.totalElements;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(currentPage, pageLimit, totalPages, totalElements);
-  }
-
-  @Override public String toString() {
-    return "PageInfo{" +
-        "currentPage=" + currentPage +
-        ", pageLimit=" + pageLimit +
-        ", totalPages=" + totalPages +
-        ", totalElements=" + totalElements +
-        '}';
   }
 }
