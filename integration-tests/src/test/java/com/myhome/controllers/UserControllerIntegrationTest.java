3b119579229b7f60254c823bb8826ae4bd23921e
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-02-23 00:29:08 +0300

    Issue #196 fix integration test

--- integration-tests/src/test/java/com/myhome/controllers/UserControllerIntegrationTest.java
+++ integration-tests/src/test/java/com/myhome/controllers/UserControllerIntegrationTest.java
@@ -12,14 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import testConfig.MailTestConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
+@Import(MailTestConfig.class)
 @SpringBootTest(
     classes = MyHomeServiceApplication.class,
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
